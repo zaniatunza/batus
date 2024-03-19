5eeefab55bbaebf4d63ef38a852f7f3dd971fa3f
zaniatunza <164097607+zaniatunza@users.noreply.github.com>
2024-03-20 05:48:17 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-532b67ca-bae5-4176-82a2-993ca51156a9/)
 
 
 echo "$curl_output" >> Update.tsx
